53204bcbbaffac9fbbfb07fe0ed14864dc3fd315
Eagleigor <ishulika@tapestrysolutions.com>
2013-11-18 11:31:18 -0800

    Added WildFly as an example server deployment

--- src/main/java/org/openapplications/oagis/_10/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.openapplications.org/oagis/10")
-package org.openapplications.oagis._10;
--- src/main/java/org/soaeasy/ws/oagis/itemmaster/_10/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * OAGIS version 10 - ItemMaster Web Service
- * 
- */
-package org.soaeasy.ws.oagis.itemmaster._10;
